08c58a5822f49486dda3f099e5b5029159b3209a
Niki Powell <nhe.powell@gmail.com>
2024-08-05 10:13:05 -0500

    logo on nav

--- components/navbar.js
+++ components/navbar.js
@@ -1,20 +1,27 @@
 import Link from "next/link.js"
+import Image from "next/image.js"
+import logo from "../public/TMS-logo.jpg"
 
 export default function Navbar() {
 
 
   return (
-      <nav className="bg-gray-800 p-12 text-white flex justify-between items-center">
+      <nav className="bg-gray-800 p-2 text-white flex justify-between items-center">
         <div>
-          <p className="text-xl font-bold">TMS Logo</p>
+          <Image src={logo} alt="TMS Logo" width={200} />
         </div>
-        <div className="flex space-x-8">
-          <Link href='/' className="no-underline text-white hover:text-gray-400">HOME</Link>
-          <Link href="/culture" className="no-underline text-white hover:text-gray-400">CULTURE</Link>
-          <Link href="/services" className="no-underline text-white hover:text-gray-400">SERVICES</Link>
-          <Link href="/portfolio" className="no-underline text-white hover:text-gray-400">PORTFOLIO</Link>
-          <Link href="/careers" className="no-underline text-white hover:text-gray-400">CAREERS</Link>
-          <Link href="/inquiry" className="no-underline text-white hover:text-gray-400">INQUIRY</Link>
+        <div className="flex items-center space-x-4">
+            <Link href="/" passHref><span className="no-underline hover:text-gray-400 cursor-pointer">HOME</span></Link>
+                <span className="text-red-500 font-bold text-xl pb-1">|</span>
+            <Link href="/culture" passHref><span className="no-underline hover:text-gray-400 cursor-pointer">CULTURE</span></Link>
+                <span className="text-red-500 font-bold text-xl pb-1">|</span>
+            <Link href="/services" passHref><span className="no-underline hover:text-gray-400 cursor-pointer">SERVICES</span></Link>
+                <span className="text-red-500 font-bold text-xl pb-1">|</span>
+            <Link href="/portfolio" passHref><span className="no-underline hover:text-gray-400 cursor-pointer">PORTFOLIO</span></Link>
+                <span className="text-red-500 font-bold text-xl pb-1">|</span>
+            <Link href="/careers" passHref><span className="no-underline hover:text-gray-400 cursor-pointer">CAREERS</span></Link>
+                <span className="text-red-500 font-bold text-xl pb-1">|</span>
+            <Link href="/inquiry" passHref><span className="no-underline hover:text-gray-400 cursor-pointer">INQUIRY</span></Link>
         </div>
       </nav>
     )
