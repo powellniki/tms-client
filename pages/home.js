a91048f9f844d1525897204a9ac502668fc95c8f
Niki Powell <nhe.powell@gmail.com>
2024-08-08 16:36:39 -0500

    our impact

--- pages/home.js
+++ pages/home.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/navbar.js"
 import Head from "next/head.js"
 import Image from "next/image.js"
 import heroImage1 from "../public/hero-1.jpeg"
+import impactImage from "../public/hero-2.jpeg"
 import RotatingText from "@/components/rotatingText.js"
 
 export default function Home() {
@@ -28,10 +29,42 @@ export default function Home() {
                         alt="Hero Image" 
                         priority
                     />
-                    <div className="absolute inset-20 flex flex-col justify-end items-center pb-16">
+                    <div className="absolute inset-20 flex flex-col justify-center md:justify-end items-center pb-16">
                         <RotatingText />
                     </div>
                 </div>
+
+                <div className="p-12 md:p-32 justify-center text-center">
+                    <h2 className="font-heading text-xl md:text-4xl lg:text-5xl text-shadow-md">TMS LLC BUILDERS + MANAGERS IS A PREMIER COMMERCIAL GENERAL CONSTRUCTION MANAGEMENT FIRM BASED IN CLARKSVILLE, TENNESSEE. WE EXCEL IN OVERSEEING ALL ASPECTS OF CONSTRUCTION PROJECTS, ENSURING TOP-TIER QUALITY FROM START TO FINISH.</h2>
+                    <p className=" pb-0 pt-8 md:pt-12 text-sm md:text-md">Brief company descriptor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus nec augue facilisis, vel scelerisque ligula dapibus. Proin aliquet, lacus vitae tincidunt sodales, libero mi pulvinar nulla, at consectetur enim dolor et dui.</p>
+                </div>
+
+                <div className="bg-gray-200 p-8 md:p-4 flex flex-col md:flex-row">
+                    <div className="flex flex-col justify-center items-center w-full md:w-1/2 lg:p-8">
+                        <h2 className="p-4 font-heading text-4xl md:text-5xl lg:text-7xl text-tms-red text-shadow-lg">OUR IMPACT</h2>
+                        <p className="text-xs sm:text-sm md:pr-2 pt-2 md:pt-4 text-center">
+                            Brief impact statement. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus nec augue facilisis, vel scelerisque ligula dapibus. Proin aliquet, lacus vitae tincidunt sodales, libero mi pulvinar nulla, at consectetur enim dolor et dui.
+                        </p>
+                        <div className="flex flex-row p-4">
+                            <div className="pl-4 pr-4">
+                                <h3 className="text-tms-yellow text-shadow-lg text-4xl md:text-5xl lg:text-7xl font-bold">100+</h3>
+                                <p className="font-heading">PROJECTS COMPLETED</p>
+                            </div>
+                            <div className="pl-4 pr-4">
+                                <h3 className="text-tms-yellow text-shadow-lg text-4xl md:text-5xl lg:text-7xl font-bold">30+</h3>
+                                <p className="font-heading">YEARS OF EXPERIENCE</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full md:pl-4 md:pr-4 md:w-1/2 flex items-center justify-center">
+                        <Image src={impactImage} alt="TMS Builders + Managers Our Impact Image" className="object-cover" />
+                    </div>
+                </div>
+
+                <div className="p-12">
+                    <h1 className="p-4 font-heading text-4xl md:text-5xl lg:text-7xl text-tms-red text-shadow-lg">RECENT PROJECTS</h1>
+                </div>
+
             </main>
         </>
     )
