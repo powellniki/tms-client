2b9845ed734929b647e48f9e1a98f7e368ff2451
Niki Powell <nhe.powell@gmail.com>
2024-08-12 08:32:09 -0500

    impact styling

--- components/ourImpact.js
+++ components/ourImpact.js
@@ -5,22 +5,20 @@ import Image from "next/image.js";
 export default function OurImpact() {
 
     return (
-        <div className="bg-gray-200 flex flex-col lg:flex-row">
+        <div className="bg-tms-red flex flex-col lg:flex-row">
             <div className="flex flex-col w-full lg:w-1/2 items-center justify-center pt-12 lg:pt-0">
-                <h2 className="font-heading text-tms-red text-5xl md:text-5xl xl:text-6xl text-shadow-lg">OUR IMPACT</h2>
-                <p className="text-sm text-center pt-4 pl-6 pr-6 md:pt-8 md:pl-8 md:pr-8 lg:pr-8 lg:pl-8 lg:pt-8">
+                <h2 className="font-heading text-white text-5xl md:text-5xl xl:text-6xl text-shadow-lg pt-4">OUR IMPACT</h2>
+                <p className="text-white text-sm md:text-lg text-center pt-4 pl-6 pr-6 md:pt-8 md:pl-12 md:pr-12 lg:pr-20 lg:pl-20 lg:pt-8 text-shadow-light">
                     Brief impact statement. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus nec augue facilisis, vel scelerisque ligula dapibus. Proin aliquet, lacus vitae tincidunt sodales, libero mi pulvinar nulla, at consectetur enim dolor et dui.
-                    Brief impact statement. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus nec augue facilisis, vel scelerisque ligula dapibus. Proin aliquet, lacus vitae tincidunt sodales, libero mi pulvinar nulla, at consectetur enim dolor et dui.
-                
                 </p>
-                <div className="flex flex-row justify-center pt-4 pb-2 md:pb-12 lg:pb-2">
-                    <div className="p-2">
+                <div className="flex flex-row justify-center pt-4 pb-2 md:pb-12 lg:pb-2 md:mt-4">
+                    <div className="p-4">
                         <h3 className="font-sans text-tms-yellow text-shadow-lg text-5xl md:text-6xl font-bold">100+</h3>
-                        <p className="font-heading">PROJECTS COMPLETED</p>
+                        <p className="text-white font-heading text-shadow-light text-xl">PROJECTS COMPLETED</p>
                     </div>
-                    <div className="p-2">
+                    <div className="p-4">
                         <h3 className="font-sans text-tms-yellow text-shadow-lg text-5xl md:text-6xl font-bold">30+</h3>
-                        <p className="font-heading">YEARS OF EXPERIENCE</p>
+                        <p className="text-white font-heading text-shadow-light text-xl">YEARS OF EXPERIENCE</p>
                     </div>
                 </div>
             </div>
