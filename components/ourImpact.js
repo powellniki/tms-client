2c70b3ada4e033efc13c1e27909042296ecd9de9
Niki Powell <nhe.powell@gmail.com>
2024-08-12 10:02:12 -0500

    sm styling

--- components/ourImpact.js
+++ components/ourImpact.js
@@ -6,12 +6,12 @@ export default function OurImpact() {
 
     return (
         <div className="bg-tms-red flex flex-col lg:flex-row">
-            <div className="w-full lg:w-1/2 flex items-center justify-center pt-8 md:pt-0">
+            <div className="w-full lg:w-1/2 flex items-center justify-center pt-0">
                 <Image src={impactImage} alt="TMS Builders + Managers Our Impact Image" className="object-cover" />
             </div>
-            <div className="flex flex-col w-full lg:w-1/2 items-center justify-center pt-12 lg:pt-0">
+            <div className="flex flex-col w-full lg:w-1/2 items-center justify-center mt-12 mb-12 lg:m-0 ">
                 <h2 className="font-heading text-white text-5xl md:text-5xl xl:text-6xl text-shadow-lg pt-4">OUR IMPACT</h2>
-                <p className="text-white text-sm md:text-lg text-center pt-4 pl-6 pr-6 md:pt-8 md:pl-12 md:pr-12 lg:pr-20 lg:pl-20 lg:pt-8 text-shadow-lg">
+                <p className="text-white text-md md:text-lg text-center pt-4 pl-6 pr-6 md:pt-8 md:pl-12 md:pr-12 lg:pr-20 lg:pl-20 lg:pt-8 text-shadow-lg">
                     Brief impact statement. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus nec augue facilisis, vel scelerisque ligula dapibus. Proin aliquet, lacus vitae tincidunt sodales, libero mi pulvinar nulla, at consectetur enim dolor et dui.
                 </p>
                 <div className="flex flex-row justify-center pt-4 pb-2 md:pb-12 lg:pb-2 md:mt-4">
