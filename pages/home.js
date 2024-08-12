9c812f669dbff2a5ec2abaefd88e697f7424038e
Niki Powell <nhe.powell@gmail.com>
2024-08-12 09:59:47 -0500

    styling small screens

--- pages/home.js
+++ pages/home.js
@@ -39,13 +39,13 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div className="p-12 md:p-28 lg:m-28 justify-center text-center">
+                <div className="mt-12 mb-12 md:mt-0 md:mb-0 p-8 md:p-28 lg:m-28 justify-center text-center">
                     <div>
                         {/* For medium and smaller screens, show h2 and span separately */}
-                        <h2 className="text-gray-900 font-heading text-5xl xl:text-6xl md:leading-snug lg:hidden text-shadow-lg">
+                        <h2 className="text-gray-900 font-heading text-4xl md:text-5xl xl:text-6xl md:leading-snug lg:hidden text-shadow-lg">
                             TMS LLC BUILDERS + MANAGERS IS A PREMIER COMMERCIAL GENERAL CONSTRUCTION MANAGEMENT FIRM BASED IN CLARKSVILLE, TENNESSEE.
                         </h2>
-                        <span className="text-gray-900 block text-md md:text-md md:pt-8 lg:hidden">
+                        <span className="text-gray-900 block text-md md:text-md pt-8 lg:hidden">
                             We excel in overseeing all aspects of construction projects, ensuring top-tier quality from start to finish.
                         </span>
 
