a5f40252f104c8b8fc4c6803f3248d7e116ec4b1
Niki Powell <nhe.powell@gmail.com>
2024-08-14 14:35:29 -0500

    spacing

--- pages/inquiry/index.js
+++ pages/inquiry/index.js
@@ -6,15 +6,16 @@ import Link from "next/link.js"
 export default function Inquiry() {
     return (
         <main className="w-screen">
-            <div className="flex flex-col lg:flex-row lg:mt-12 mb-12">
-                <div className="bg-gray-400 h-[24rem] md:h-[28rem] lg:h-[32rem] lg:w-1/2 lg:m-8">
+
+            <div className="flex flex-col lg:flex-row p-4 md:mt-24 md:mb-24 lg:mt-32 lg:mb-32">
+                <div className="lg:w-1/2 w-full bg-gray-300 h-[32rem] lg:h-[32rem] aspect-w-16 aspect-h-9 flex items-center justify-center">
                     Image placeholder
                 </div>
-                <div className="lg:w-1/2 m-4 md:m-8 lg:m-8 lg:pt-4">
-                    <h1 className="uppercase font-heading text-tms-red text-5xl lg:text-6xl text-shadow-lg pt-8 md:pt-8 lg:pt-16">
+                <div className="lg:w-1/2 w-full mt-4 lg:mt-16 p-4 lg:p-8">
+                    <h1 className="uppercase text-tms-red font-heading text-5xl xl:text-6xl md:leading-snug text-shadow-lg mb-4 lg:mb-8">
                         General Inquiries
                     </h1>
-                    <p className="pt-8 lg:pt-12 lg:pr-4 text-md lg:text-lg">
+                    <p className="text-gray-800">
                     If you have any questions or would like to discuss your upcoming construction project, we are here to help. 
                     At TMS LLC Builders + Managers, our team is ready to provide you with the information and guidance you need. 
                     Whether you are looking for more details about our services or exploring how we can tailor a solution to 
