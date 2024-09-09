385f4b25cf4fd009098a68fb6fe21fbdb435c944
Niki Powell <nhe.powell@gmail.com>
2024-09-09 09:21:26 -0500

    updated culture text

--- pages/culture/history.js
+++ pages/culture/history.js
@@ -4,15 +4,24 @@ import Navbar from "@/components/navbar.js"
 
 export default function History() {
     return (
-        <section className="w-screen flex flex-col lg:flex-row p-4 md:mt-24 md:mb-24 lg:mt-32 lg:mb-32 lg:mx-28">
-            <div className="lg:w-1/2 w-full bg-gray-300 h-[32rem] lg:h-[32rem] lg:w-[48rem] flex items-center justify-center">
+        <section className="w-screen flex flex-col lg:flex-row p-4 md:mt-24 md:mb-24 lg:px-8">
+            <div className="w-full bg-gray-300 h-[32rem] lg:h-[32rem] lg:w-2/3 flex items-center justify-center">
                 <span className="text-white text-2xl">Image Placeholder</span>
             </div>
-            <div className="lg:w-1/2 w-full mt-4 lg:mt-16 p-4 lg:px-20">
-                <h1 className="text-tms-red font-heading text-5xl xl:text-6xl md:leading-snug mb-4 lg:mb-8 tracking-wider">OUR HISTORY</h1>
-                <h2 className="font-heading text-tms-black text-lg lg:text-xl pt-8 lg:mt-8 uppercase tracking-wider">History subheader statement goes here</h2>
-                <p className="text-gray-800 text-md md:text-lg font-Roboto font-light mt-2 lg:mt-4">
-                    TMS history description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus nec augue facilisis, vel scelerisque ligula dapibus.
+            <div className="w-full flex flex-col justify-center p-4 lg:pl-16">
+                <h1 className="text-tms-red font-heading text-5xl xl:text-6xl md:leading-snug mb-4 tracking-wider">OUR HISTORY</h1>
+                <h2 className="font-heading text-tms-black text-lg lg:text-xl pt-2 lg:mt-4 uppercase tracking-wider">Building with integrity and excellence for over 30 years</h2>
+                <p className="text-gray-800 text-md md:text-md font-Roboto font-light mt-2 lg:mt-4">
+                    For more than three decades, TMS LLC Builders + Managers has proudly shaped the commercial and industrial landscape of Middle Tennessee. From warehouses and office buildings to schools and food kitchens, our commitment to delivering high-quality design-build services has made us a trusted partner in the region.
+                </p>
+                <p className="text-gray-800 text-md md:text-md font-Roboto font-light mt-2 lg:mt-4">
+                    At the heart of TMS is a culture rooted in customer satisfaction, quality craftsmanship, and lasting relationships. Our clients know they can count on us for innovative design, timely project completion, and exceptional service. We ensure every project meets the highest quality, safety, and sustainability standards—whether building from the ground up or transforming existing spaces.
+                </p>
+                <p className="text-gray-800 text-md md:text-md font-Roboto font-light mt-2 lg:mt-4">
+                    We believe that strong client relationships are the cornerstone of our success, and we foster trust and open communication in every partnership. Just as important is the well-being of our team. By prioritizing job satisfaction and family support, we create a work environment where our employees thrive, knowing their contributions are valued.
+                </p>
+                <p className="text-gray-800 text-md md:text-md font-Roboto font-light mt-2 lg:mt-4">
+                    At TMS, we are more than builders—we are a community dedicated to excellence, care, and long-term partnerships that stand the test of time. 
                 </p>
             </div>
         </section>
