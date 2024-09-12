8f5f68ca3dc203e65b5c7f7ebdfef67691516716
Niki Powell <nhe.powell@gmail.com>
2024-09-12 17:03:43 -0500

    inquiries image with spacing

--- pages/careers/index.js
+++ pages/careers/index.js
@@ -1,25 +1,36 @@
 import Layout from "@/components/layout.js"
 import Navbar from "@/components/navbar.js"
 import Image from "next/image.js"
+import careerImage from "/public/careers-inquiry.png"
 
 
 export default function Careers() {
     return (
         <main className="w-screen">
-
-            <div className="w-screen flex flex-col lg:flex-row p-4 md:mt-24 md:mb-24 lg:px-8">
-                <div className="hidden md:block bg-gray-300 h-[32rem] lg:h-[32rem] lg:w-2/3 flex items-center justify-center">
-                    <Image />
+            <div className="flex flex-col lg:flex-row p-4 md:mt-16 md:mb-24 lg:px-8">
+    
+                {/* Image Container */}
+                <div className="w-full lg:w-1/2 h-[50vh] md:h-[60vh] lg:h-[75vh] overflow-hidden relative">
+                    <Image
+                    src={careerImage}
+                    alt="Careers at TMS"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="left center"
+                    className="rounded-sm"
+                    />
                 </div>
-                <div className="w-full flex flex-col justify-center p-4 lg:pl-16">
+        
+                {/* Text Content */}
+                <div className="w-full lg:w-1/2 flex flex-col justify-center p-4 lg:pl-16">
                     <h1 className="text-tms-red font-heading text-5xl xl:text-6xl md:leading-snug mb-4 tracking-wider uppercase">Careers at TMS</h1>
                     <h2 className="font-heading text-tms-black text-lg lg:text-xl pt-2 lg:mt-4 uppercase tracking-wider">Build a rewarding career with a leading construction management firm</h2>
                     <p className="text-gray-800 text-md lg:text-lg font-Roboto font-light mt-2 lg:mt-4">
-                        At TMS LLC Builders + Managers, we are always looking for talented, dedicated professionals 
-                        to join our team. We offer a dynamic work environment where you can grow and thrive, working 
-                        on exciting projects that make a real impact. Explore our open career opportunities and 
-                        discover how you can contribute to our commitment to excellence in construction. Apply today 
-                        and take the first step toward a fulfilling career with TMS.
+                    At TMS LLC Builders + Managers, we are always looking for talented, dedicated professionals 
+                    to join our team. We offer a dynamic work environment where you can grow and thrive, working 
+                    on exciting projects that make a real impact. Explore our open career opportunities and 
+                    discover how you can contribute to our commitment to excellence in construction. Apply today 
+                    and take the first step toward a fulfilling career with TMS.
                     </p>
                 </div>
             </div>
--- pages/inquiry/thankyou.js
+++ pages/inquiry/thankyou.js
@@ -14,17 +14,17 @@ export default function InquiryThankYou() {
     return (
         <main className="w-screen flex items-center justify-center">
             <div className="flex flex-col text-center px-4 mt-24 md:mt-52 lg:mt-56">
-                <h1 className="uppercase font-heading text-center text-tms-red text-5xl lg:text-6xl text-shadow-lg mb-8">
+                <h1 className="uppercase tracking-wider font-heading text-center text-tms-red text-5xl lg:text-6xl mb-8">
                     Thank You For Your Inquiry
                 </h1>
-                <p className="text-md md:text-lg lg:text-xl mt-4 md:mt-8 lg:mt-12 px-4 md:px-8 lg:mx-40">
-                    Thank you for your inquiry! We have successfully received your submission and appreciate your interest 
+                <p className="text-gray-800 text-md lg:text-lg font-Roboto font-light mt-2 lg:mt-4 w-2/3 mx-auto">
+                    We have successfully received your submission and appreciate your interest 
                     in TMS LLC Builders + Managers. One of our representatives will review your information and get back to 
                     you shortly. We look forward to discussing how we can assist with your construction needs.
                 </p>
                 <button
                     onClick={navigateHome}
-                    className="uppercase font-heading font-bold text-xl bg-tms-yellow text-tms-white py-3 w-48 hover:bg-tms-black hover:text-tms-white text-shadow-sm mt-8 lg:mt-12 mx-auto"
+                    className="uppercase tracking-wider font-heading font-bold text-xl bg-tms-yellow text-tms-white py-3 w-48 hover:bg-tms-black hover:text-tms-white text-shadow-sm mt-8 lg:mt-12 mx-auto"
                 >
                     return home
                 </button>
