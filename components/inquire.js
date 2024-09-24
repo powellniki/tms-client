48250ab533df62f7339a4b46c887f306a46f7218
Niki Powell <nhe.powell@gmail.com>
2024-09-24 14:23:46 -0500

    inquiry component text and spacing

--- components/inquire.js
+++ components/inquire.js
@@ -33,16 +33,16 @@ export default function Inquire() {
       }
 
     return (
-        <div className="bg-tms-gray text-center mt-32 pt-20 pb-20 px-4 md:px-0">
+        <div className="bg-tms-gray text-center mt-32 py-28 px-4 md:px-0">
             <div ref={inquireRef}>
-                <h2 className="font-heading w-2/3 lg:w-1/2 mx-auto text-tms-white text-3xl lg:text-4xl pt-4 tracking-wider text-shadow-sm uppercase">explore the possibilities for your upcoming building project</h2>
-                <h3 className="font-heading text-tms-white text-xl lg:text-2xl mt-14 uppercase tracking-wider"></h3>
-                <p className="text-tms-white font-Roboto font-light text-md lg:text-lg text-center mx-auto mt-4 w-2/3 lg:w-1/2">
-                    Our dedicated team at TMS, LLC is here to provide you with expert guidance and tailored solutions to meet your construction needs.
+                <h2 className="font-heading w-2/3 lg:w-1/2 mx-auto text-tms-white text-3xl lg:text-4xl tracking-wider text-shadow-sm uppercase">explore the possibilities for your upcoming building project</h2>
+                {/* <h3 className="font-heading text-tms-white text-xl lg:text-2xl mt-14 uppercase tracking-wider"></h3> */}
+                <p className="text-tms-white font-Roboto font-light text-md lg:text-lg text-center mx-auto mt-6 w-2/3 lg:w-1/2">
+                    Our dedicated team at TMS Builders + Managers is here to provide you with expert guidance and tailored solutions to meet your construction needs.
                 </p>
-                <p className="text-tms-white w-2/3 lg:w-1/2 mx-auto font-Roboto font-light text-md lg:text-lg text-center mt-6">
+                {/* <p className="text-tms-white w-2/3 lg:w-1/2 mx-auto font-Roboto font-regular text-md lg:text-lg text-center mt-4">
                     Contact us today to discuss how we can bring your vision to life.
-                </p>
+                </p> */}
                 <button 
                     onClick={handleNavigation}
                     className="uppercase font-heading text-xl bg-tms-yellow text-tms-white py-3 w-48 hover:bg-tms-black hover:text-tms-white text-shadow-sm mt-6 lg:mt-8"
