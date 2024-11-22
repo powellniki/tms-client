c9482b1d9ebf0d3d46008bb4fde29f4b82d35fb0
Niki Powell <nhe.powell@gmail.com>
2024-11-22 10:01:57 -0600

    update safety blurb

--- pages/culture/safety.js
+++ pages/culture/safety.js
@@ -24,7 +24,10 @@ export default function Safety() {
                 <h1 className="text-tms-red font-heading text-5xl xl:text-6xl md:leading-snug mt-4 lg:mt-0 mb-4 tracking-wider uppercase">OUR SAFETY</h1>
                 <h2 className="font-heading text-tms-black text-lg lg:text-xl pt-2 lg:mt-4 uppercase tracking-wider">At TMS Builders + Managers, we are committed to safety above all</h2>
                 <p className="text-gray-800 text-md lg:text-lg font-Roboto font-light mt-2 lg:mt-4">
-                    Safety is our utmost priority at TMS due to the inherent risks and hazards in construction activities. Our comprehensive safety management protocols are designed to prevent accidents, injuries, and fatalities on the job site, ensuring the health and well-being of our workers. Maintaining strict compliance with legal and regulatory requirements fosters a strong safety culture within our company and on our job sites, enhancing productivity and, most importantly, protecting our employees.
+                    TMS Builders + Managers is committed to maintaining the highest safety standards in all construction and remodeling projects. We take a proactive
+                    approach to create secure, efficient work environments by cultivating a strong culture of safety. Our team works closely with subcontractors to ensure 
+                    safety practices are fully integrated into their operations. This collaborative effort guarantees that every project not only meets but surpasses 
+                    industry standards, delivering exceptional results and providing our clients with confidence and peace of mind.
                 </p>
             </div>
         </section>
